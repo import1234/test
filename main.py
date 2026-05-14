1ee8ff3cd1f3a6ad73590e67906dade42305b6f5
import1234 <162861691+import1234@users.noreply.github.com>
2026-05-14 12:22:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -69,38 +69,34 @@ wait = WebDriverWait(driver, 1)
 # ==========================================
 # 🚀 DOM 파싱 루프 (타이머 적용)
 # ==========================================
-send_discord("🚀 닌텐도 뮤지엄 감시 시작", mode="all")
 
 driver.get(MAIN_URL)
 time.sleep(2)
+
+send_discord("# 🚀 닌텐도 뮤지엄 감시 시작", mode="log")
 
 loop_count = 0
 
 # GitHub Actions 6시간 제한을 대비한 시작 시간 기록
 start_time = datetime.now(ZoneInfo("Asia/Seoul"))
-max_duration = timedelta(hours=5, minutes=53)
+max_duration = timedelta(hours=5, minutes=55)
 
 while True:
     now = datetime.now(ZoneInfo("Asia/Seoul"))
     loop_count += 1
     timestamp = now.strftime('%H:%M:%S')
 
-    # ⏱️ 5시간 50분 경과 시 안전 종료
+    # ⏱️ 5시간 55분 경과 시 안전 종료
     if now - start_time > max_duration:
         send_discord("⏱️ [시스템] GitHub Actions 타임아웃 임박. 릴레이를 위해 스크립트를 안전 종료합니다.", mode="log")
         break
 
     try:
         # 1. 5월 탭 찾아서 클릭
-        try:
-            may_tab_xpath = "//span[contains(text(), 'May')]/ancestor::a[contains(@class, 'p-period__month')]"
-            may_tab = wait.until(EC.element_to_be_clickable((By.XPATH, may_tab_xpath)))
-            may_tab.click()
-            time.sleep(0.2)
-        except:
-            driver.refresh()
-            time.sleep(1)
-            continue
+        may_tab_xpath = "//span[contains(text(), 'May')]/ancestor::a[contains(@class, 'p-period__month')]"
+        may_tab = wait.until(EC.element_to_be_clickable((By.XPATH, may_tab_xpath)))
+        may_tab.click()
+        time.sleep(0.2)
 
         available_dates = []
 
@@ -122,7 +118,7 @@ while True:
             send_discord(f"🚨🎉 [발견] {available_dates} 예매 가능!!\n👉 {MAIN_URL}", mode="all")
             break
         else:
-            if loop_count % 50 == 0:
+            if loop_count % 10 == 0:
                 send_discord(f"-# [{timestamp}] {loop_count}회차 감시 중... 전부 매진", mode="log")
 
         time.sleep(random.uniform(4.0, 9.0))
